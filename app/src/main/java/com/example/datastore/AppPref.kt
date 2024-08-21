22fe25042dc08d86150e0bcd2822f13a1dcd2f23
AyazAktaş <ayazaktastr@gmail.com>
2024-08-21 16:00:14 +0300

    Data Store'a kayıt , sil, oku fonk.

--- app/src/main/java/com/example/datastore/AppPref.kt
+++ app/src/main/java/com/example/datastore/AppPref.kt
@@ -1,6 +1,83 @@
 package com.example.datastore
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.doublePreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
 
 class AppPref(var context:Context) {
+    val Context.ds : DataStore<Preferences> by preferencesDataStore("bilgiler")
+
+    companion object{
+        val AD_KEY = stringPreferencesKey("AD")
+        val YAS_KEY = intPreferencesKey("YAS")
+        val BOY_KEY = doublePreferencesKey("BOY")
+        val BEKAR_KEY = booleanPreferencesKey("BEKAR")
+        val ARKADAS_LISTE_KEY = stringSetPreferencesKey("ARKADAS_LISTE")
+    }
+
+    suspend fun kayitAl(ad:String){
+        context.ds.edit {
+            it[AD_KEY]=ad
+        }
+    }
+    suspend fun okuAd():String{
+       val p=context.ds.data.first()
+        return p[AD_KEY]?:"isim gelmedi!"
+    }
+    suspend fun silAd(){
+        context.ds.edit {
+            it.remove(AD_KEY)
+        }
+    }
+    suspend fun kayitYas(yas:Int){
+        context.ds.edit {
+            it[YAS_KEY]=yas
+        }
+    }
+    suspend fun okuYas():Int{
+        val p=context.ds.data.first()
+        return p[YAS_KEY]?:0
+    }
+
+    suspend fun kayitBoy(boy:Double){
+        context.ds.edit {
+            it[BOY_KEY]=boy
+        }
+    }
+    suspend fun okuBoy():Double{
+        val p=context.ds.data.first()
+        return p[BOY_KEY]?:0.0
+    }
+
+
+    suspend fun kayitBekar(bekar:Boolean){
+        context.ds.edit {
+            it[BEKAR_KEY]=bekar
+        }
+    }
+    suspend fun okuBekar():Boolean{
+        val p=context.ds.data.first()
+        return p[BEKAR_KEY]?:true
+    }
+
+    suspend fun kayitArkadasListe(arkadasListe:Set<String>){
+        context.ds.edit {
+            it[ARKADAS_LISTE_KEY]=arkadasListe
+        }
+    }
+    suspend fun okuArkadasListe():Set<String>?{
+        val p=context.ds.data.first()
+        return p[ARKADAS_LISTE_KEY]
+    }
+
+
+
 }
--- app/src/main/java/com/example/datastore/MainActivity.kt
+++ app/src/main/java/com/example/datastore/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.datastore
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
+        val ap =AppPref(this)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            //Kayıt
+            ap.kayitAl("ayaz")
+            val gelenAd=ap.okuAd()
+            Log.e("Okunan Isim: ",gelenAd)
+        }
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
